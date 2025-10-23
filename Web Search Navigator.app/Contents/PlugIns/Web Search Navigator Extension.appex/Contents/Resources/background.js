746abefac832612c28487665c18eb5b04206acf3
xcritty <xuchen@infini-ai.com>
2025-10-23 23:46:49 +0800

    remove files that are not

--- Web Search Navigator.app/Contents/PlugIns/Web Search Navigator Extension.appex/Contents/Resources/background.js
+++
@@ -1,9 +0,0 @@
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'tabsCreate') {
-    browser.tabs.create({
-      url: request.options.url,
-      active: request.options.active,
-      openerTabId: sender.tab.id,
-    });
-  }
-});
